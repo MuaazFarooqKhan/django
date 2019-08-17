470c99972da6f360b9b153bf14fdf63300fe70b2
Muaaz Farooq Khan <kmuaaz18@gmail.com>
2019-08-17 10:43:21 -0700

    latest

--- bussystem/models.py
+++ bussystem/models.py
@@ -1,14 +1,14 @@
 from django.core.validators import RegexValidator
 from django.db import models
-# from django.db import models
-# from django.contrib.auth.base_user import AbstractBaseUser
-# from django.contrib.auth.base_user import BaseUserManager
-# from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
 class Company(models.Model):
-    # com_id = models.AutoField(primary_key=True, unique=True)
-    adminfirstname = models.CharField(max_length=150, null=True)
+    com_id = models.AutoField(primary_key=True)
+    adminfirstname = models.CharField(max_length=150, null=True, unique=True)
     adminlastname = models.CharField(max_length=150, null=True)
     companyname = models.CharField(max_length=150, null=True, unique=True)
     companyemail = models.CharField(max_length=150, null=True, unique=True)
@@ -18,19 +18,18 @@ class Company(models.Model):
     def __str__(self):
 
         if self.companyname == None:
-            temp = "Company no: "+str(id)
-            return temp
+            return "ERROR-CUSTOMER NAME IS NULL"
         return self.companyname
 
 
 class Customer(models.Model):
     # alphanumeric = RegexValidator(r'^[a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
     # name = models.CharField(max_length=50, blank=True, null=True, validators=[alphanumeric])
-    # cus_foreign = models.ForeignKey(Company, on_delete=models.CASCADE)
+    cus_foreign = models.ForeignKey(Company, on_delete=models.CASCADE, null=False, blank=False)
     cus_id = models.AutoField(primary_key=True)
-    firstname = models.CharField(max_length=50)
+    firstname = models.CharField(max_length=50, unique=True)
     lastname = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=150)
     confirmpassword = models.CharField(max_length=150)
 
@@ -42,10 +41,10 @@ class Bt(models.Model):
     # name = models.CharField(max_length=50, blank=True, null=True, validators=[alphanumeric])
     # bt_for = models.ForeignKey(Company, on_delete=models.CASCADE)
     # id = models.AutoField(primary_key=True)
-    companyidd = models.ForeignKey(Company, on_delete=models.CASCADE)
-    bt_id = models.AutoField(primary_key=True)
+    company_for = models.ForeignKey(Company, on_delete=models.CASCADE, null=False, blank=False)
+    bt_id = models.AutoField(primary_key=True, unique=True)
     username = models.CharField(max_length=150, null=True)
-    email = models.CharField(max_length=150, null=True)
+    email = models.CharField(max_length=150, null=True, unique=True)
     fnames = models.CharField(max_length=150, null=True)
     lnames = models.CharField(max_length=150, null=True)
     address = models.CharField(max_length=150, null=True)
@@ -53,39 +52,34 @@ class Bt(models.Model):
     passwords = models.CharField(max_length=150, null=True)
 
     def __str__(self):
-        return self.email
+        return self.bt_id
 
 class Station(models.Model):
     companyid = models.ForeignKey(Company, on_delete=models.CASCADE)
-    stationname = models.CharField(max_length=150, null=True)
+    stationname = models.CharField(max_length=150, null=True, unique=True)
 
     def __str__(self):
         return self.stationname
 
 class BusDetail(models.Model):
-    bsid = models.ForeignKey(Company, on_delete=models.CASCADE)
-    busno = models.CharField(max_length=150, null=False)
-    busplatenumber = models.CharField(max_length=150, null=False)
+    busno = models.CharField(max_length=150, null=True, unique=True)
     # Type = models.CharField(max_length=150, null=True)
 
     def __str__(self):
-        return self.busno
+        return self.Type
 
 
-class routee(models.Model):
-    to_route = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='but_to_fore', null=False, blank=False)
-    from_route = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='bus_froom_fore', null=False, blank=False)
-    company_name = models.ForeignKey(Company, on_delete=models.CASCADE, null=False, blank=False)
-    bus_details = models.ForeignKey(BusDetail, on_delete=models.CASCADE, null=False, blank=False)
-    bus_no = models.CharField(max_length=150, null=False)
-    Time = models.CharField(max_length=150, null=True)
-    price = models.FloatField(max_length=8, null=True)
+class Route(models.Model):
+    to = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='but_to_fore' ,null=False, blank=False)
+    fromm = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='bus_froom_fore', null=False, blank=False)
+    com = models.ForeignKey(Company, on_delete=models.CASCADE, null=False, blank=False)
+    bs = models.ForeignKey(BusDetail, on_delete=models.CASCADE, null=False, blank=False)
+    busno = models.CharField(max_length=150,null=True)
+    # Time = models.CharField(max_length=150,null=True)
+    # price = models.FloatField(max_length=8,null=True)
 
     def __str__(self):
-        if self.bus_no == None:
-            return "ERROR-Busno IS NULL"
-        return self.bus_no
-
+        return self.busno
 
 class xt(models.Model):
     x_time=models.DateTimeField(null=True)
--- bussystem/views.py
+++ bussystem/views.py
@@ -1,16 +1,24 @@
 from __future__ import unicode_literals
-
-from pprint import pprint
-
 from django.contrib import messages
 from django.shortcuts import render, redirect, HttpResponse
 from django.db import IntegrityError
 # Create your views here.
-from bussystem.models import Customer, Bt, Company, Station, routee, BusDetail
+from bussystem.models import Customer, Bt, Company, Station, Route
 from fontawesome_5.fields import IconField
 from datetime import datetime
 
 
+def deletebt(request):
+    bts = Bt.objects.all()
+    context = {
+        'bts': bts
+    }
+    return render(request, 'deletebt.html', context)
+
+def delete(request, id):
+    bt = Bt.objects.get(pk=id)
+    bt.delete()
+    return redirect('/deletebt')
 
 # def deletebt(request):
 #     return render(request, 'deletebt.html')
@@ -62,7 +70,7 @@ def authentication(request):
         # request.session['password']=Cust_password
         return render(request, 'BtTicket.html')
     elif (com > 0):
-        request.session['email'] = companyemail
+        request.session['email'] = email
         # request.session['password']=Cust_password
         return render(request, 'adminprofile.html')
     else:
@@ -85,7 +93,6 @@ def signout(request):
 
 
 def home(request):
-
     return render(request, 'home.html')
 
 
@@ -109,12 +116,9 @@ def companyregister(request):
                               companyemail=companyemail, companypassword=companypassword,
                               companyconfirmpassword=companyconfirmpassword)
     company_details.save()
-    # messages.success(request, 'You are  Already Loged In')
+    messages.success(request, 'You are  Already Loged In')
     return render(request, 'companyregister.html')
 
-def callcompanyregister(request):
-    # print(request.POST)
-    return render(request, 'companyregister.html')
 
 
 def btticket(request):
@@ -130,163 +134,65 @@ def usercancel(request):
 
 
 def userticket(request):
-    stations = Station.objects.all()
-    context = {
-        'stations': stations
-    }
-    return render(request, 'userticket.html', context)
+    return render(request, 'userticket.html')
 
 
 def station(request):
     print(request.POST)
     stationname = request.GET.get('stationname')
-    email = request.session['email']
-
-    if request.session.get('email'):
-        new_station = Company.objects.get(companyemail=email)
-
-    station_details = Station(companyid=new_station, stationname=stationname)
+    station_details = Station(stationname=stationname)
     station_details.save()
-    # sta = Station.objects.all()
-    # context = {
-    #     'sta': sta
-    # }
     return render(request, 'AddThings.html')
+
+
+def routes(request):
+    print(request.POST)
+    myDate = datetime.now()
+    busno = request.GET.get('busno')
+    Time = request.GET.get('Time')
+    price = request.GET.get('price')
+    routes_details= Route(busno=busno, Time=Time, price= price)
+    routes_details.save()
+    return render(request, 'AddThings.html', {
+        'myDate': myDate
+    })
 
 
 def createbt(request):
     print(request.POST)
     username = request.GET.get('username')
-    eemail = request.GET.get('email')
+    email = request.GET.get('email')
     fnames = request.GET.get('fnames')
     lnames = request.GET.get('lnames')
     address = request.GET.get('address')
     city = request.GET.get('city')
     passwords = request.GET.get('passwords')
-    email = request.session['email']
 
-    if request.session.get('email'):
-        newbusno = Company.objects.get(companyemail=email)
-
-    if not (Bt.objects.filter(email=eemail).count()>0 and eemail != ''):
-        bt_details = Bt(companyidd= newbusno, username=username, email=eemail, fnames=fnames, lnames=lnames, address=address,
-                        city=city, passwords=passwords)
-
-        bt_details.save()
-
+    bt_details = Bt(username=username, email=email, fnames=fnames, lnames=lnames, address=address,
+                    city=city, passwords=passwords)
+    bt_details.save()
     return render(request, 'createbt.html')
     # return redirect('/')
 
 
 
-def deletebt(request):
-    bts = Bt.objects.all()
-    context = {
-        'bts': bts
-    }
-    return render(request, 'deletebt.html', context)
-
-def delete(request, id):
-    bt = Bt.objects.get(pk=id)
-    bt.delete()
-    return redirect('/deletebt')
-
-
-
-def adminprofile(request, pk=None):
-    email = request.session['email']
-    if request.session.get('email'):
-        newbusno = Company.objects.get(companyemail=email)
-        com_username=newbusno.adminfirstname
-        com_userlastname=newbusno.adminlastname
-        com_name=newbusno.companyname
-        companyemail=newbusno.companyemail
-        context = {
-            'com_name':com_name,
-            'com_username':com_username,
-            'com_userlastname':com_userlastname,
-            'companyemail':companyemail,
-        }
-
-    return render(request, 'adminprofile.html', context)
+def adminprofile(request):
+    return render(request, 'adminprofile.html')
 
 
 def performance(request):
     return render(request, 'performance.html')
 
 
-def update(request):
-    # email = request.session['email']
-    #
-    # if request.session.get('email'):
-    #     books = Company.objects.get(str(pk=id))
-    #     context = {
-    #         'books': books
-    #     }
-    return render(request, 'update.html')
-
-
-def edit(request):
-    # books = Company.objects.get(pk=id)
-    # books.title = request.GET['title']
-    return redirect('/')
-
-
 def addthings(request):
-    busno = request.GET.get('seats')
-    busplatenumber = request.GET.get('platenumber')
-    email = request.session['email']
-
-
-    if request.session.get('email'):
-        newbusno = Company.objects.get(companyemail=email)
-        com_name=newbusno.companyname
-        com_id = Company.objects.get(companyemail=email).id
-        mystations=Station.objects.filter(companyid=com_id)
-        context = {
-            'mystations': mystations,
-            'com_name':com_name
-        }
-    routes_details = BusDetail(bsid=newbusno, busno=busno, busplatenumber=busplatenumber)
-    routes_details.save()
-    return render(request, 'addthings.html',context)
-
-
-def calladdthings(request):
-    email = request.session['email']
-    if request.session.get('email'):
-        com_id = Company.objects.get(companyemail=email).id
-        mystations=Station.objects.filter(companyid=com_id)
-        rou=routee.objects.filter()
-
-
-        context = {
-            'mystations': mystations,
-            'rou':rou,
-        }
-    return render(request, 'addthings.html',context)
-
-
-def routes(request):
-    print(request.POST)
-    busno= request.GET.get('busno')
-    Time = request.GET.get('Time')
-    price = request.GET.get('price')
-    email = request.session['email']
-
-    if request.session.get('email'):
-        newbus = Company.objects.get(companyemail=email)
-
-
-
-    routes_details= routee(com=newbus, busno=busno, Time=Time, price= price)
-    routes_details.save()
-    return render(request, 'AddThings.html')
+    # print(request.POST)
+    # fnames = request.GET.get('fnames')
+    # lnames = request.GET.get('lnames')
+    # bus_details = Bt(busno=username, Type=email, fnames=fnames, lnames=lnames, address=address,
+    #                 city=city, passwords=passwords)
+    # bus_details.save()
+    return render(request, 'addthings.html')
 
 
 def adminsearch(request):
     return render(request, 'adminsearch.html')
-
-
-def bookticket(request):
-    return render(request, 'bookticket.html')
