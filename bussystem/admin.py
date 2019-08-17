c4b0a086ff3c5655c6e53c612c5ca79197293c95
Muaaz Farooq Khan <kmuaaz18@gmail.com>
2019-08-17 10:38:08 -0700

    updated somepages but stillsome issues to solve

--- bussystem/admin.py
+++ bussystem/admin.py
@@ -7,7 +7,7 @@ admin.site.register(Customer)
 admin.site.register(Bt)
 admin.site.register(Company)
 admin.site.register(Station)
-admin.site.register(Route)
+admin.site.register(routee)
 admin.site.register(BusDetail)
 admin.site.register(xt)
 
