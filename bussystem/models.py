3fb7fc70a60a17a7f4cd1ee08066f43279e0118f
Muaaz Farooq Khan <kmuaaz18@gmail.com>
2019-08-23 06:30:12 -0700

    23-8-2019

--- bussystem/models.py
+++ bussystem/models.py
@@ -1,14 +1,14 @@
 from django.core.validators import RegexValidator
 from django.db import models
-from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import PermissionsMixin
+# from django.db import models
+# from django.contrib.auth.base_user import AbstractBaseUser
+# from django.contrib.auth.base_user import BaseUserManager
+# from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
 class Company(models.Model):
-    com_id = models.AutoField(primary_key=True)
-    adminfirstname = models.CharField(max_length=150, null=True, unique=True)
+    # com_id = models.AutoField(primary_key=True, unique=True)
+    adminfirstname = models.CharField(max_length=150, null=True)
     adminlastname = models.CharField(max_length=150, null=True)
     companyname = models.CharField(max_length=150, null=True, unique=True)
     companyemail = models.CharField(max_length=150, null=True, unique=True)
@@ -18,18 +18,19 @@ class Company(models.Model):
     def __str__(self):
 
         if self.companyname == None:
-            return "ERROR-CUSTOMER NAME IS NULL"
+            temp = "Company no: "+str(id)
+            return temp
         return self.companyname
 
 
 class Customer(models.Model):
     # alphanumeric = RegexValidator(r'^[a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
     # name = models.CharField(max_length=50, blank=True, null=True, validators=[alphanumeric])
-    cus_foreign = models.ForeignKey(Company, on_delete=models.CASCADE, null=False, blank=False)
+    # cus_foreign = models.ForeignKey(Company, on_delete=models.CASCADE)
     cus_id = models.AutoField(primary_key=True)
-    firstname = models.CharField(max_length=50, unique=True)
+    firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=100)
-    email = models.CharField(max_length=100, unique=True)
+    email = models.CharField(max_length=100)
     password = models.CharField(max_length=150)
     confirmpassword = models.CharField(max_length=150)
 
@@ -41,10 +42,10 @@ class Bt(models.Model):
     # name = models.CharField(max_length=50, blank=True, null=True, validators=[alphanumeric])
     # bt_for = models.ForeignKey(Company, on_delete=models.CASCADE)
     # id = models.AutoField(primary_key=True)
-    company_for = models.ForeignKey(Company, on_delete=models.CASCADE, null=False, blank=False)
-    bt_id = models.AutoField(primary_key=True, unique=True)
+    companyidd = models.ForeignKey(Company, on_delete=models.CASCADE)
+    bt_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=150, null=True)
-    email = models.CharField(max_length=150, null=True, unique=True)
+    email = models.CharField(max_length=150, null=True)
     fnames = models.CharField(max_length=150, null=True)
     lnames = models.CharField(max_length=150, null=True)
     address = models.CharField(max_length=150, null=True)
@@ -52,34 +53,39 @@ class Bt(models.Model):
     passwords = models.CharField(max_length=150, null=True)
 
     def __str__(self):
-        return self.bt_id
+        return self.email
 
 class Station(models.Model):
     companyid = models.ForeignKey(Company, on_delete=models.CASCADE)
-    stationname = models.CharField(max_length=150, null=True, unique=True)
+    stationname = models.CharField(max_length=150, null=True)
 
     def __str__(self):
         return self.stationname
 
 class BusDetail(models.Model):
-    busno = models.CharField(max_length=150, null=True, unique=True)
+    bsid = models.ForeignKey(Company, on_delete=models.CASCADE)
+    busno = models.CharField(max_length=150, null=False)
+    busplatenumber = models.CharField(max_length=150, null=False)
     # Type = models.CharField(max_length=150, null=True)
 
     def __str__(self):
-        return self.Type
+        return self.busno
 
 
-class Route(models.Model):
-    to = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='but_to_fore' ,null=False, blank=False)
-    fromm = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='bus_froom_fore', null=False, blank=False)
-    com = models.ForeignKey(Company, on_delete=models.CASCADE, null=False, blank=False)
-    bs = models.ForeignKey(BusDetail, on_delete=models.CASCADE, null=False, blank=False)
-    busno = models.CharField(max_length=150,null=True)
-    # Time = models.CharField(max_length=150,null=True)
-    # price = models.FloatField(max_length=8,null=True)
+class routee(models.Model):
+    to_route = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='but_to_fore', null=False, blank=False)
+    from_route = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='bus_froom_fore', null=False, blank=False)
+    company_name = models.ForeignKey(Company, on_delete=models.CASCADE, null=False, blank=False)
+    bus_details = models.ForeignKey(BusDetail, on_delete=models.CASCADE, null=False, blank=False)
+    bus_no = models.CharField(max_length=150, null=False)
+    Time = models.CharField(max_length=150, null=True)
+    price = models.FloatField(max_length=8, null=True)
 
     def __str__(self):
-        return self.busno
+        if self.bus_no == None:
+            return "ERROR-Busno IS NULL"
+        return self.bus_no
+
 
 class xt(models.Model):
     x_time=models.DateTimeField(null=True)
--- bussystem/urls.py
+++ bussystem/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('registrationforuser/', views.registrationforuser, name='registrationforuser'),
     path('companyregister/', views.companyregister, name='companyregister'),
+    path('callcompanyregister/', views.callcompanyregister, name='callcompanyregister'),
     path('create/', views.create, name='create'),
     path('authentication/', views.authentication, name='authentication'),
     path('btticket/', views.btticket, name='btticket'),
@@ -19,9 +20,13 @@ urlpatterns = [
     path('adminprofile/', views.adminprofile, name='adminprofile'),
     path('performance/', views.performance, name='performance'),
     path('addthings/', views.addthings, name='addthings'),
+    path('calladdthings/', views.calladdthings, name='calladdthings'),
     path('routes/', views.routes, name='routes'),
     path('station/', views.station, name='station'),
     path('adminsearch/', views.adminsearch, name='adminsearch'),
     path('signout/', views.signout, name='signout'),
+    path('update/', views.update, name='update'),
+    path('bookticket/', views.bookticket, name='bookticket'),
+    path('edit/<id>/', views.edit, name='edit'),
 
 ]
